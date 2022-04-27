d7117a7701afb64e0d7e71840b6571763d4227a1
Bryan Leow Xuan Zhen <blxz96@gmail.com>
2022-04-28 00:18:32 +0800

    Completed miniproject_3

--- Parallel Programming in Java/miniproject_3/src/main/java/edu/coursera/parallel/MatrixMultiply.java
+++ Parallel Programming in Java/miniproject_3/src/main/java/edu/coursera/parallel/MatrixMultiply.java
@@ -1,6 +1,6 @@
 package edu.coursera.parallel;
 
-import static edu.rice.pcdp.PCDP.forseq2d;
+import static edu.rice.pcdp.PCDP.*;
 
 /**
  * Wrapper class for implementing matrix multiply efficiently in parallel.
@@ -20,8 +20,7 @@ public final class MatrixMultiply {
      * @param C The output matrix
      * @param N Size of each dimension of the input matrices
      */
-    public static void seqMatrixMultiply(final double[][] A, final double[][] B,
-            final double[][] C, final int N) {
+    public static void seqMatrixMultiply(final double[][] A, final double[][] B, final double[][] C, final int N) {
         forseq2d(0, N - 1, 0, N - 1, (i, j) -> {
             C[i][j] = 0.0;
             for (int k = 0; k < N; k++) {
@@ -44,7 +43,7 @@ public final class MatrixMultiply {
          * TODO Parallelize this outermost two-dimension sequential loop to
          * achieve performance improvement.
          */
-        forseq2d(0, N - 1, 0, N - 1, (i, j) -> {
+        forall2dChunked(0, N - 1, 0, N - 1, (i, j) -> {
             C[i][j] = 0.0;
             for (int k = 0; k < N; k++) {
                 C[i][j] += A[i][k] * B[k][j];
